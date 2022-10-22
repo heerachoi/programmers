110c115293b9320b66965042e39e65c4d0806dfd
heerachoi <heera7722@gmail.com>
2022-10-22 09:56:40 +0900

    다음에 올 숫자

---
+++ 다음에올숫자.js
@@ -0,0 +1,10 @@
+// 문제 링크 & 코드 리뷰: https://codesign.tistory.com/207
+
+function solution(common) {
+  let n1 = common[0],
+    n2 = common[1],
+    n3 = common[2];
+  return n2 - n1 === n3 - n2
+    ? common[common.length - 1] + n2 - n1
+    : common[common.length - 1] * (n2 / n1);
+}
