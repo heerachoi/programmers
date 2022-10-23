d86a9075d2d3dddcb55d09ad0ccbf47c39f0fa64
heerachoi <heera7722@gmail.com>
2022-10-23 22:07:23 +0900

    Update 다음에올숫자.js

--- 다음에올숫자.js
+++ 다음에올숫자.js
@@ -4,7 +4,7 @@ function solution(common) {
   let n1 = common[0],
     n2 = common[1],
     n3 = common[2];
-  return n2 - n1 === n3 - n2
+  return n2 - n1 === n3 - n2               
     ? common[common.length - 1] + n2 - n1
     : common[common.length - 1] * (n2 / n1);
 }
