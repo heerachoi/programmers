7455aa2ab745664e7b591f761953162b39433bad
heerachoi <heera7722@gmail.com>
2022-11-06 10:04:54 +0900

    python questions

---
+++ Python/두수의나눗셈.py
@@ -0,0 +1,4 @@
+# 문제 링크 & 코드 리뷰: https://codesign.tistory.com/253
+
+def solution(num1, num2):
+    return int(num1 / num2 * 1000)
---
+++ Python/배열자르기.py
@@ -0,0 +1,4 @@
+# 문제 링크 & 코드 리뷰: https://codesign.tistory.com/253
+
+def solution(numbers, num1, num2):
+    return numbers[num1:num2+1]
