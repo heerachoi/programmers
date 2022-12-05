ef74da59e875a4387d9427aadee715b51b26fe90
heerachoi <heera7722@gmail.com>
2022-12-05 09:51:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,3 @@
-function solution(n)
-{
-    return [...(n+"")].reduce((acc, val,index) => acc + parseInt(val), 0);
+function solution(n) {
+    return (n+"").split("").reduce((acc, curr) => acc + parseInt(curr), 0)
 }
