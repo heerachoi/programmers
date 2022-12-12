509928dd2a2f0b41c9119ff15fe630e9ebc92ba9
heerachoi <heera7722@gmail.com>
2022-12-12 13:04:02 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,12 +1,8 @@
 function solution(num) {
     var answer = 0;
-    while(num > 1) {
-        if (num % 2 == 0) {
-            num = num/2;
-        } else {
-            num = num*3 + 1;
-        }
+    while(num != 1 && answer != 500){
+        num%2 == 0 ? num = num/2 : num = num*3 +1;
         answer++;
     }
-    return (answer < 500) ? answer : -1;
+    return num == 1 ? answer : -1;
 }
