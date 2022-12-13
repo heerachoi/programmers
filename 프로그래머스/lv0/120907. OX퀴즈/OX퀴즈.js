89d80fa9ff32c7f8357a89190fbaa07504e5b2fb
heerachoi <heera7722@gmail.com>
2022-12-13 19:25:07 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,20 +1,10 @@
 function solution(quiz) {
     var answer = [];
-    for (let i = 0; i < quiz.length; i++) {
-        let formula = quiz[i].split(" ");
-        if (formula[1] == '-') {
-            if (Number(formula[0]) - Number(formula[2]) == Number(formula[4])) {
-                answer.push('O');
-            } else {
-                answer.push('X');
-            }
-        } else {
-            if (Number(formula[0]) + Number(formula[2]) == Number(formula[4])) {
-                answer.push('O');
-            } else {
-                answer.push('X');
-            }
-        }
-    }
-    return answer;
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
 }
--- 프로그래머스/lv0/120907. OX퀴즈/README.md
+++ 프로그래머스/lv0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.06 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
