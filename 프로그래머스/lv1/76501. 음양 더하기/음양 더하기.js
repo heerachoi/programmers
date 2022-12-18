1925c9931a55cfb4742b66a8d57a7206a47f328e
heerachoi <heera7722@gmail.com>
2022-12-18 10:43:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,4 +1,3 @@
 function solution(absolutes, signs) {
-    var answer = absolutes.reduce((acc,curr,index) => acc + (curr * (signs[index] ? 1 : -1)), 0);
-    return answer;
+    return absolutes.reduce((acc,curr,index) => acc + (curr * (signs[index] ? 1 : -1)), 0);
 }
