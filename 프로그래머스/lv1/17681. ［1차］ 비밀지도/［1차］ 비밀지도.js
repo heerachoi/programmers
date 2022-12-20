7a03d4a2d4382fc99f54e0e2cdb323058706adbc
heerachoi <heera7722@gmail.com>
2022-12-20 10:25:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,24 +1,23 @@
-function solution(n, arr1, arr2) {
+function solution(n, arr1, arr2) { // 9:20
     var answer = Array(n).fill("");
-
     for (let i = 0; i < arr1.length; i++) {
-        var radix1 = arr1[i].toString(2);
-        var radix2 = arr2[i].toString(2);
-
-        var final1 = radix1.split("");
-        var final2 = radix2.split("");
-
-        while (final1.length < n) {
-            final1.unshift("0");
+        let str1 = "";
+        let str2 = "";
+        let num1 = arr1[i].toString(2);
+        let num2 = arr2[i].toString(2);
+        if (num1.length < n) {
+            let len1 = num1.split("").length;
+            num1 = "0".repeat(n-len1) + num1;
         }
-        while (final2.length < n) {
-            final2.unshift("0");
+        if (num2.length < n) {
+            let len2 = num2.split("").length;
+            num2 = "0".repeat(n-len2) + num2;
         }
 
-        for (let j = 0; j <= n; j++) {
-            if (final1[j] == '1' || final2[j] == '1') {
+        for (let j = 0; j < n; j++) {
+            if (num1[j] == "1" || num2[j] == "1") {
                 answer[i] += "#";
-            } else if (final1[j] == '0' || final2[j] == '0') {
+            } else {
                 answer[i] += " ";
             }
         }
