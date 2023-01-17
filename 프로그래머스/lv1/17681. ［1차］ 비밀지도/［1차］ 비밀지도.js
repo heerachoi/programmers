560c4f0275e86616d47b4897b3990c0992d2d525
heerachoi <heera7722@gmail.com>
2023-01-17 12:18:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,26 +1,8 @@
-function solution(n, arr1, arr2) { // 9:20
-    var answer = Array(n).fill("");
-    for (let i = 0; i < arr1.length; i++) {
-        let str1 = "";
-        let str2 = "";
-        let num1 = arr1[i].toString(2);
-        let num2 = arr2[i].toString(2);
-        if (num1.length < n) {
-            let len1 = num1.split("").length;
-            num1 = "0".repeat(n-len1) + num1;
-        }
-        if (num2.length < n) {
-            let len2 = num2.split("").length;
-            num2 = "0".repeat(n-len2) + num2;
-        }
+function solution(n, arr1, arr2) {
+    return arr1.map((v,i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a=>+a ? '#':" "));
 
-        for (let j = 0; j < n; j++) {
-            if (num1[j] == "1" || num2[j] == "1") {
-                answer[i] += "#";
-            } else {
-                answer[i] += " ";
-            }
-        }
-    }
-    return answer;
+}
+
+const addZero = (n,s) => {
+    return '0'.repeat(n-s.length) + s;
 }
