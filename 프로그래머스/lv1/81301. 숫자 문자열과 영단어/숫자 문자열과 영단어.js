cc19d5f7e265a2a6e0bf95ed6d6c952c5e6acb6b
heerachoi <heera7722@gmail.com>
2023-01-19 11:05:56 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,7 @@
 function solution(s) {
     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    for (let i = 0; i < numbers.length; i++) {
-        s = s.split(numbers[i]).join(i);
-    }
+   for (let i = 0; i < 10; i++) {
+       s = s.split(numbers[i]).join(i);
+   }
     return Number(s);
 }
