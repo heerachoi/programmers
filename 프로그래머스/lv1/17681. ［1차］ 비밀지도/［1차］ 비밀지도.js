e213fe58163374fe64f3f093bc18c5d03ea9036d
heerachoi <heera7722@gmail.com>
2023-01-19 11:02:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,8 +1,4 @@
 function solution(n, arr1, arr2) {
-    return arr1.map((v,i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a=>+a ? '#':" "));
-
+    return answer = arr1.map((a,i) => (a|arr2[i]).toString(2).padStart(n,0).replace(/1|0/g, v => +v ? '#' : ' '));
 }
 
-const addZero = (n,s) => {
-    return '0'.repeat(n-s.length) + s;
-}
