7bb662fad2b1e3aadb974979c23e7d6def135096
heerachoi <heera7722@gmail.com>
2023-01-25 22:59:56 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,20 +1,12 @@
 function solution(keyinput, board) {
-    var answer = [0,0];
-    let keys = {
-        left: [-1,0],
-        right: [1,0],
-        up: [0,1],
-        down: [0,-1],
+    let res = [0,0];
+    for (let p of keyinput) {
+        switch(p){
+            case 'left': if (-res[0] < board[0]/2-1) res[0]--; break;
+            case 'right': if (res[0] < board[0]/2-1) res[0]++; break;
+            case 'up': if (res[1] < board[1]/2-1) res[1]++; break;
+            case 'down': if (-res[1] < board[1]/2-1) res[1]--; break;
+        }
     }
-    for (let i = 0; i < keyinput.length; i++) {
-        if((answer[0]+keys[keyinput[i]][0] <= Math.floor(board[0]/2)) && 
-            (answer[0]+keys[keyinput[i]][0] >= (-1 *Math.floor(board[0]/2))) &&
-            (answer[1]+keys[keyinput[i]][1] <= Math.floor(board[1]/2)) &&
-            (answer[1]+keys[keyinput[i]][1] >= (-1 *Math.floor(board[1]/2)))
-           ) {
-            answer[0]+=keys[keyinput[i]][0]
-            answer[1]+=keys[keyinput[i]][1]
-            }
-    }
-    return answer;
+    return res;
 }
