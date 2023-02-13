91b13591fd799495e31c49ee49f365056824f398
heerachoi <heera7722@gmail.com>
2023-02-13 23:33:30 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,5 @@
-function solution(s){
-  return "*".repeat(s.length - 4) + s.slice(-4);
+
+
+function solution(phone_number) {
+    return '*'.repeat(phone_number.length-4)+phone_number.slice(-4);
 }
